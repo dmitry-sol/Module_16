61bed8f80162372499b02f07cd1d2fb5dbfaeb48
dmitry-sol <dmitry-sol@mail.ru>
2024-12-17 10:29:58 +1100

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -3,19 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-def read_root():
-    return {"message": "Hello, World!"}
+def read_root() -> str:
+    return "Главная Страница"
 
 @app.get("/user/admin")
-def read_admin():
-    return {"message": "Вы вошли как администратор"}
+def read_admin() -> str:
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def read_user(user_id: str):
-    return {"message": f'Вы вошли как пользователь № {user_id}'}
+async def read_user(user_id: int) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user/")
-def get_user_info(username: str, age: str):
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
-# uvicorn module_16_1:app --reload
+def get_user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
