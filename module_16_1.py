d86f5bf2e78950928dd7051f5a7caba239833bb7
dmitry-sol <dmitry-sol@mail.ru>
2024-12-17 03:51:47 +1100

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/user/admin")
+def read_admin():
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"message": f'Вы вошли как пользователь № {user_id}'}
+
+@app.get("/user/")
+def get_user_info(username: str, age: int):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+# uvicorn module_16_1:app --reload
